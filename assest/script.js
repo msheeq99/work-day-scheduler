331c0b31dba5998d63db5d939553723feb5f5b84
Mohamud <msheeq99@outlook.com>
2022-10-27 13:50:34 +0100

    add all my hours from the loacal storage with getItem

--- assest/script.js
+++ assest/script.js
@@ -49,7 +49,18 @@ $(document).ready(function () {
 
 
 
-// ge
+// get items from local storage if any
 
+$("#hour-09 .description").val(localStorage.getItem("hour-09"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+timeTracker();
 
 })
