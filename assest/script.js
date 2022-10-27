17bdfa9c21f3faa6e65da5c4ea801cc20be5ec8c
Mohamud <msheeq99@outlook.com>
2022-10-27 13:17:00 +0100

    saved text in the local storage

--- assest/script.js
+++ assest/script.js
@@ -1,4 +1,27 @@
 // display today's day and date.
-var todayDate = moment().format('dddd, MMM Do YYYY');
+const todayDate = moment().format('dddd, MMM Do YYYY, H:mm');
 $("#currentDay").html(todayDate);
 
+// array time blocks
+const hoursArray = [
+    $("#9"),
+    $("#10"),
+    $("#11"),
+    $("#12"),
+    $("#13"),
+    $("#14"),
+    $("#15"),
+    $("#16"),
+    $("#17")
+];
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text); 
+    })
